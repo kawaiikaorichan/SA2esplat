c17cfaf758ad95d89e453edfb4950f8d0c9a9646
kawaiikaorichan <super_so_nic1@hotmail.com>
2025-03-10 15:32:05 -0600

    Se añade código para el remplazo de los mensajes por obtención de puntos.

--- SA2esplat/Mod.cpp
+++ SA2esplat/Mod.cpp
@@ -12,6 +12,30 @@ enum Doblaje { Neutro, Mexicano, Chileno, Argentino };
 static bool MusicaDub = true;
 static int Dub = Neutro;
 
+DataArray(NJS_TEXANIM, TechPoints, 0x173B440, 11);
+
+NJS_TEXANIM TechPointsSpanish[] = {
+   { 85, 19, 0, 9, 166, 4, 252, 23, 16, 0 }, //2000
+   { 86, 19, 0, 9, 4, 4, 90, 23, 16, 0 }, //Good
+   { 134, 20, 0, 10, 4, 29, 138, 49, 16, 0 }, //Nice
+   { 102, 20, 0, 10, 4, 54, 106, 74, 16, 0 }, //Great
+   { 155, 21, 0, 11, 4, 78, 159, 99, 16, 0 }, //Jammin'
+   { 144, 20, 0, 10, 4, 105, 118, 125, 16, 0 }, //Cool
+   { 131, 20, 0, 10, 4, 130, 135, 150, 16, 0 }, //Radical
+   { 108, 19, 0, 9, 4, 156, 112, 175, 16, 0 }, //Tight
+   { 205, 20, 0, 10, 4, 181, 209, 201, 16, 0 }, //Awesome
+   { 153, 22, 0, 11, 4, 206, 157, 226, 16, 0 }, //Extreme
+   { 154, 20, 0, 10, 4, 231, 158, 251, 16, 0 } //Perfect
+};
+
+void ReplaceTechPoints()
+{
+	for (int i = 0; i < 11; i++)
+	{
+		TechPoints[i] = TechPointsSpanish[i];
+	}
+}
+
 extern "C"
 {
 	__declspec(dllexport) void Init(const char* path, const HelperFunctions& helperFunctions)
@@ -89,7 +113,39 @@ extern "C"
 
 		SetNewGunRoboTitles();
 		InitOtherBossTitles();
+		ReplaceTechPoints();
 
+		//Interfaz
+		helperFunctions.ReplaceTexture("batadvPlayer", "2P_chara_en02", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\2P_chara_en02.png"), 9000703, 256, 256);
+		helperFunctions.ReplaceTexture("bossAttack", "bossat_window", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\bossat_window.png"), 9001510, 512, 512);
+		helperFunctions.ReplaceTexture("console", "h_teckpoint_all", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\h_teckpoint_all.png"), 100000000, 512, 512);
+		helperFunctions.ReplaceTexture("console_dc", "h_teckpoint_all", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\h_teckpoint_all.png"), 100000000, 512, 512);
+		helperFunctions.ReplaceTexture("console_gc", "h_teckpoint_all", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\h_teckpoint_all.png"), 100000000, 512, 512);
+		helperFunctions.ReplaceTexture("course", "matome01", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\matome01.png"), 10000000, 256, 256);
+		helperFunctions.ReplaceTexture("coursdc", "matome01", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\matome01.png"), 10000000, 256, 256);
+		helperFunctions.ReplaceTexture("coursgc", "matome01", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\matome01.png"), 10000000, 256, 256);
+		helperFunctions.ReplaceTexture("kartRace", "kartrace_waku0", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\kartrace_waku0.png"), 9000816, 128, 128);
+		helperFunctions.ReplaceTexture("objtex_common", "sikake_10_128", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\sikake_10_128.png"), 4009, 128, 128);
+		helperFunctions.ReplaceTexture("objtex_commondc", "sikake_10_128", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\sikake_10_128.png"), 4009, 128, 128);
+		helperFunctions.ReplaceTexture("objtex_commongc", "sikake_10_128", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\sikake_10_128.png"), 4009, 128, 128);
+		helperFunctions.ReplaceTexture("result", "h_result_all", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\Resultados.png"), 11000010, 256, 256);
+		helperFunctions.ReplaceTexture("stageMap", "stg_info_spr", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\stg_info_spr.png"), 9000500, 256, 256);
+		helperFunctions.ReplaceTexture("stageMap", "stg_infox_asc", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\stg_infox_asc.png"), 9000515, 256, 128);
+		helperFunctions.ReplaceTexture("stageMap", "stg_info_asc", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\stg_info_asc.png"), 9000514, 256, 256);
+		helperFunctions.ReplaceTexture("stageMap", "stg_stagename", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\stg_stagename.png"), 8000011, 1024, 1024);
+		helperFunctions.ReplaceTexture("missiontex_eg", "mission_numall", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\mission_numall.png"), 87006007, 256, 256);
+		helperFunctions.ReplaceTexture("missiontex_kn", "mission_numall", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\mission_numall.png"), 87006007, 256, 256);
+		helperFunctions.ReplaceTexture("missiontex_ro", "mission_numall", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\mission_numall.png"), 87006007, 256, 256);
+		helperFunctions.ReplaceTexture("missiontex_sh", "mission_numall", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\mission_numall.png"), 87006007, 256, 256);
+		helperFunctions.ReplaceTexture("missiontex_so", "mission_numall", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\mission_numall.png"), 87006007, 256, 256);
+		helperFunctions.ReplaceTexture("missiontex_ta", "mission_numall", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\mission_numall.png"), 87006007, 256, 256);
+		//Texturas niveles
+		helperFunctions.ReplaceTexture("course", "miu128_rh007", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\anillos.png"), 90019, 128, 128);
+		helperFunctions.ReplaceTexture("coursdc", "miu128_rh007", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\anillos.png"), 90019, 128, 128);
+		helperFunctions.ReplaceTexture("coursgc", "miu128_rh007", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\anillos.png"), 90019, 128, 128);
+		helperFunctions.ReplaceTexture("course", "miu128_rh014", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\kaori.png"), 90020, 128, 128);
+		helperFunctions.ReplaceTexture("coursdc", "miu128_rh014", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\kaori.png"), 90020, 128, 128);
+		helperFunctions.ReplaceTexture("coursgc", "miu128_rh014", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\kaori.png"), 90020, 128, 128);
 		helperFunctions.ReplaceTexture("objtex_stg14", "miu128_rh007", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\anillos.png"), 13507, 128, 128);
 		helperFunctions.ReplaceTexture("objtex_stg14", "miu128_rh014", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\kaori.png"), 13514, 128, 128);
 		helperFunctions.ReplaceTexture("objtex_stg52", "miu128_rh014", helperFunctions.GetReplaceablePath("resource\\gd_PC\\Texturas\\kaori.png"), 10501, 128, 128);
